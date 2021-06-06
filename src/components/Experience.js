3c175b18cd0c94a6188d1694cb451353eb324935
Polocatik12 <pona4kakot@gmail.com>
2021-06-06 10:50:28 +0200

    Experience

---
+++ src/components/Experience.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5">
+                <h1> experience</h1>
+            </div>
+            <div className="container experience-wraper">
+                <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                     <div className="timeline-content">
+                         <h3>2014-2015</h3>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                             Ratione assumenda voluptate, aliquid voluptas molestias ad blanditiis facilis dolore quod similique voluptatibus minus,
+                              dolorem ea fugiat laudantium nesciunt doloremque corrupti saepe! 
+                             Laborum ipsam quibusdam aliquid aut a blanditiis eligendi sit?</p>
+                     </div>
+                </div>
+                
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                     <div className="timeline-content">
+                         <h3>2015-2016</h3>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                             Ratione assumenda voluptate, aliquid voluptas molestias ad blanditiis facilis dolore quod similique voluptatibus minus,
+                              dolorem ea fugiat laudantium nesciunt doloremque corrupti saepe! 
+                             Laborum ipsam quibusdam aliquid aut a blanditiis eligendi sit?</p>
+                     </div>
+                </div>
+                
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                     <div className="timeline-content">
+                         <h3>2016-2017</h3>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                             Ratione assumenda voluptate, aliquid voluptas molestias ad blanditiis facilis dolore quod similique voluptatibus minus,
+                              dolorem ea fugiat laudantium nesciunt doloremque corrupti saepe! 
+                             Laborum ipsam quibusdam aliquid aut a blanditiis eligendi sit?</p>
+                     </div>
+                </div>
+                
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                     <div className="timeline-content">
+                         <h3>2017-2018</h3>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                             Ratione assumenda voluptate, aliquid voluptas molestias ad blanditiis facilis dolore quod similique voluptatibus minus,
+                              dolorem ea fugiat laudantium nesciunt doloremque corrupti saepe! 
+                             Laborum ipsam quibusdam aliquid aut a blanditiis eligendi sit?</p>
+                     </div>
+                </div>
+                
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                     <div className="timeline-content">
+                         <h3>2018-2019</h3>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                             Ratione assumenda voluptate, aliquid voluptas molestias ad blanditiis facilis dolore quod similique voluptatibus minus,
+                              dolorem ea fugiat laudantium nesciunt doloremque corrupti saepe! 
+                             Laborum ipsam quibusdam aliquid aut a blanditiis eligendi sit?</p>
+                     </div>
+                </div>
+                
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                     <div className="timeline-content">
+                         <h3>2019-2020</h3>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                             Ratione assumenda voluptate, aliquid voluptas molestias ad blanditiis facilis dolore quod similique voluptatibus minus,
+                              dolorem ea fugiat laudantium nesciunt doloremque corrupti saepe! 
+                             Laborum ipsam quibusdam aliquid aut a blanditiis eligendi sit?</p>
+                     </div>
+                </div>
+                
+            </div>
+        </div> 
+    )
+}
+
+export default Experience
